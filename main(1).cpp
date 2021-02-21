2b1485f46da731347a1137d3e6adf57de968b7a5
Yilin99999 <68358884+Yilin99999@users.noreply.github.com>
2021-02-21 23:36:52 +0800

    Update main(1).cpp

--- main(1).cpp
+++ main(1).cpp
@@ -1,12 +1,36 @@
-#include <iostream>
 #include "Player.h"
-int main() {
-    Player player;
-    player.BuildCity();
-    player.DestroyArmy()
-    player.MoveArmies();
-    player.PayCoin();
-    player.PlaceNewArmies();
-    player.MoveOverLand();
-    return 0;
+#include <iostream>
+
+int main()
+{
+	Territory* territory1 = new Territory();
+	Territory* territory2 = new Territory("Troy");
+	Player* player1 = new Player("Achilles", 12);
+
+	std::cout << "\n Creat a new army:\t";
+	player1->placeNewArmies(territory1);
+	Army* army = player1->getArmies()->front();
+    
+	std::cout << "\n Moving army:\t";
+	player1->moveArmies(army, territory2);
+
+	std::cout << "\n Moving army overland:\t";
+	player1->moveOverLand(army, territory1);
+
+	std::cout << "\n Find territory1 owner:\t";
+	territory1->getOwner();
+
+	std::cout << "\n Find territory2 owner:\t";
+	territory2->getOwner();
+
+	std::cout << "\n Destory army:\t";
+	player1->destroyArmy(army);
+	
+	std::cout << "\n" << std::endl;
+
+	territory1 = nullptr;
+	territory2 = nullptr;
+	player1 = nullptr;
+
+	return 0;
 }
