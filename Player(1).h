800e4e0f84481792c869f1bbaafbd5a26b1ec172
Yilin99999 <68358884+Yilin99999@users.noreply.github.com>
2021-02-21 23:00:05 +0800

    Update Player(1).h

--- Player(1).h
+++ Player(1).h
@@ -2,31 +2,87 @@
 #ifndef PROJECT_PLAYER_H
 #define PROJECT_PLAYER_H
 
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
 #include <iostream>
 #include "Map.h"
+#include "BiddingFacility.h"
 #include "Card.h"
 #include "Biding.h"
 using namespace std;
+using std::string;
+
 class Player {
+    
+    private:
+	string name;
+	int numOfCoins;
+	int numOfCubes;
+	int numOfDisks;
+	BiddingFacility* bidFaci;
+	list<Territory*> playerTerritory;
+	list<Card*> playerHand;
+	list<City*> cities;
+	list<Army*> armies;
 
 public:
+    Player();
+	Player(string name, int coinNum);
+	Player(const Player&);
+	~Player();
     Map map;
     Card card;
     Biding biding;
-    void PayCoin();
-    void PlaceNewArmies();
-    void MoveArmies();
-    void MoveOverLand();
-    void BuildCity();
-    void DestroyArmy();
-    string getName()//need to implement
-    int getCoins()//need to implement
-    bidding *getBidFacility()//need to implement
+	bool payCoin(int& cost);
+	void placeNewArmies(Territory* territory);
+	void moveArmies(Army*, Territory*);
+	void moveOverLand(Army*, Territory*);
+	void buildCity(Territory* territory);
+	void destroyArmy(Army*);
+	string getName();
+	void setName(string n);
+	int getCoins();
+	void setCoins(int c);
+	BiddingFacility* getBidFaci() const;
+	list<Army*>* getArmies();
     
-    Player();
+  class Army
+{
 
-    Player(const Map &map, const Card &card, const Biding &biding);
+    private:
+	    Player* owner;
+	    Territory* position;
+
+
+    public:
+	    Army();
+    	Army(Player*, Territory*);
+	    Army(const Army&);
+	    ~Army();
+	    string getOwnerName();
+	    Player* getOwner();
+	    Territory* getPosition();
+    	void setPosition(Territory*);
+    	Army& operator=(Army&);
 };
 
+class City
+{
+	Player* owner;
+	Territory* position;
+    public:
+	       City();
+	       City(Player*, Territory*);
+           City(const City&);
+           ~City();
+	// Useful methods.
+	string getOwnerName();
+	Player* getOwner();
+	Territory* getPosition();
+	City& operator=(City&);
 
-#endif //PROJECT_PLAYER_H
+    };
+};
+#endif
