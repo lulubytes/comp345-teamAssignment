07c03c642a8c6f309cdcf61a65dbdc3a8eea358d
Yilin99999 <68358884+Yilin99999@users.noreply.github.com>
2021-02-21 22:51:34 +0800

    Update Player(1).cpp

--- Player(1).cpp
+++ Player(1).cpp
@@ -1,6 +1,18 @@
 #include "Player.h"
 
+#include <iostream>
+#include "Cards/Cards.h"
+#include "BiddingFacility.h"
+using namespace std;
+
+
 void Player::PayCoin() {
+    if(){
+        
+    }
+    else{
+        
+    }
     cout << "This is PayCoin" << endl;
 }
 
@@ -23,8 +35,65 @@ void Player::DestroyArmy() {
     cout << "This is DestroyArmy" << endl;
 }
 
-Player::Player() {}
+//constructor
+Player::Player() {
+	                name = "Player1";
+	                numOfCoins = 0;
+	                bidFaci = new BiddingFacility();
+	                playerTerritory = list<Territory*>();
+	                playerHand = list<Card*>();
+                	cities = list<City*>();
+	                armies = list<Army*>();
+	                numOfCubes = 18;
+	                numOfDisks = 3;
+                    }
 
-Player::Player(const Map &map, const Card &card, const Biding &biding) : map(map), card(card), biding(biding) {}
+Player::Player(string Name, int Coinnum) {
+    
+	                                        name = Name;
+	                                        numOfCoins = Coinnum;
+	                                        bidFaci = new BiddingFacility();
+                                        	playerTerritory = list<Territory*>();
+                                        	playerHand = list<Card*>();
+                                          	cities = list<City*>();
+                                        	armies = list<Army*>();
+	                                        numOfCubes = 18;
+	                                        numOfDisks = 3;
+}
+
+Player::Player(const Player& anotherPlayer)
+{
+	name = anotherPlayer.name;
+	numOfCoins = anotherPlayer.numOfCoins;
+
+	if (anotherPlayer.bidFaci)
+		bidFaci = new BiddingFacility(*(anotherPlayer.bidFaci));
+	else
+		bidFaci = nullptr;
+
+	// Shallow copy because the map is the same.
+	for (Territory* territory : anotherPlayer.playerTerritory)
+	{
+		playerTerritory.push_back(territory);
+	}
+
+	for (Card* card : anotherPlayer.playerHand)
+	{
+		playerHand.push_back(new Card(*card));
+	}
+
+	for (City* city : anotherPlayer.cities)
+	{
+		cities.push_back(new City(*city));
+	}
+
+	for (Army* army : anotherPlayer.armies)
+	{
+		armies.push_back(new Army(*army));
+	}
+
+	numOfCubes = anotherPlayer.numOfCubes;
+	numOfDisks = anotherPlayer.numOfDisks;
+}
 
 
