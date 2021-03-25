c6958b86e9998fd04b88eb1155b761ce1585d34b
yuzhuzhai <56008059+yuzhuzhai@users.noreply.github.com>
2021-03-24 22:22:15 -0400

    Add files via upload

---
+++ A2/Cards.h
@@ -0,0 +1,97 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+//card
+class Card
+{
+public:
+	//constructor
+	string good;
+	string action[2];
+
+	string ability;
+	string andor;
+
+
+	Card();
+
+	~Card();
+	//copy constructor
+	Card(const Card& c);
+	Card(string good, string action, string andor, string ability);
+	Card(string good, string firstAction, string secondAction, string andor, string ability);
+
+
+	//assignment operators
+	Card& operator = (const Card& c);
+	//Accessors
+	string getGood();
+	string getAction1();
+	string getAction2();
+
+	string getAndor();
+
+
+	string getAbility();
+
+	void printCard();
+
+	friend ostream& operator<<(ostream&, const Card&);
+};
+
+//deck
+class Deck {
+public:
+
+	//Constructor
+	Deck();//Default
+	Deck(int numPlayer);
+	//Copy constructor
+	Deck(const Deck& d);
+	//Deconstructor
+	~Deck();
+	void generateDeck();
+	void printDeck();
+	void shuffleDeck();
+	Card* draw();
+	Deck& operator=(const Deck&);
+
+	static int  myRandomGenerator(int j);
+	int getNumCards();
+
+private:
+	Card* topCardptr;
+	Card cards[34];
+	int numPlayer;
+
+	//Output
+
+	friend ostream& operator<<(ostream&, const Deck&);
+};
+
+//hand
+class Hand {
+private:
+	Deck* deck;
+	Card* cards[6];
+public:
+	Hand() = default;
+	Hand(Deck* deck);
+	//CopyConstructor
+	Hand(const Hand& h);
+	//Deconstructor
+	~Hand();
+	int getCardCost(int indexOfcard);
+	Card* exchange(int cardIndex);
+	void printHand();
+	void shiftCards(int index);
+
+	//Output
+	friend ostream& operator<<(ostream&, const Hand&);
+	Hand& operator=(const Hand&);
+};
